bd06ed03838742f9209e072cf4539c2a2979e793
Manuel Johannes Messner <manuel.johannes.messner@hs-furtwangen.de>
2014-04-28 09:56:42 +0200

    Added trim function

--- c/gen/h2c.pl
+++ c/gen/h2c.pl
@@ -4,3 +4,10 @@ use strict;
 use warnings;
 use Data::Dumper;               # for debugging
 # use Getopt::Long;             # not yet needed
+
+sub trim {
+    $_ = shift // $_;
+    s/^\s*//;
+    s/\s*$//;
+    return $_;
+}
